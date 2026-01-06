220fece6cf00174b20b55b1f293384a498db2cc4
Mike Tak <shentai77@gmail.com>
2026-01-06 17:13:09 +0800

    專案設定檔設定完成

--- library-common/src/test/java/com/library/common/LibraryCommonApplicationTests.java
+++ library-common/src/test/java/com/library/common/LibraryCommonApplicationTests.java
@@ -1,13 +1,13 @@
-package com.library.library;
+package com.library.common;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class LibraryApplicationTests {
+class LibraryCommonApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
