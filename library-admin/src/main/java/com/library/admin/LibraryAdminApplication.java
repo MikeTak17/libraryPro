3e6551e6b2cba59725885ce4099b17c984508087
MikeTak17 <shentai77@gmail.com>
2026-02-14 22:50:54 +0800

    完成X Spring File Storage整合

--- library-admin/src/main/java/com/library/admin/LibraryAdminApplication.java
+++ library-admin/src/main/java/com/library/admin/LibraryAdminApplication.java
@@ -1,11 +1,13 @@
 package com.library.admin;
 
+import cn.xuyanwu.spring.file.storage.spring.EnableFileStorage;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @MapperScan("com.library.**.mapper")
 @SpringBootApplication(scanBasePackages = {"com.library.*"})
+@EnableFileStorage
 public class LibraryAdminApplication {
 
     public static void main(String[] args) {
